b9d4137cb08054db97d9475213b4ed4cb670da94
Haoqi Gu <55101972+H40Q1@users.noreply.github.com>
2021-03-30 15:54:53 -0400

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -5,31 +5,20 @@ import sys
 import cv2
 from difference_cal import Difference
 import numpy as np
-
+from confusion_mat import ConfMat
 
 def main():
-    # data
-    path1 ="../data/img1.jpg"  # test case path
-    path2 ="../data/img2.jpg"
+    threshold = [[0.1, 0.2, 0.3, 0.4],
+                 [0.2, 0.3, 0.4, 0.5],
+                 [0.1, 0.2, 0.3, 0.4],
+                 [0.1, 0.15, 0.2, 0.3],
+                 [5, 10, 20, 40],
+                 [0.001, 0.01, 0.1, 0.2],
+                 [0.1, 0.15, 0.2, 0.3]
+                 ]
 
-    img1 = cv2.imread(path1)  # test case (432, 768, 3)
-    img2 = cv2.imread(path2)
-
-    diff = Difference(img1, img2)
-
-    # print all 7 differences
-    print("Difference-1 Image Difference :", diff.img_diff)
-    print("Difference-2 RGB Histogram Difference :", diff.rgb_hist)
-    print("Difference-3 HSV Histogram Difference :", diff.hsv_hist)
-    print("Difference-4 Gradient Direction Histogram Difference :", diff.hog_diff)
-    print("Difference 5: Hausdorff Distance :", diff.hau_diff)
-    print("Difference 6: Cosine Similarity :", diff.cos_diff)
-    print("Difference 7: Structural Similarity Index Measure :", diff.ssim_diff)
-
-    # print("Difference 6: Local Edge Distance :", diff.loc_diff)
-    # print("Difference 7: Invariant Movement Distance :", diff.inv_diff)
-
-
+    mat = ConfMat(threshold)
+    tpr, fpr = mat.tpr, mat.fpr
 
 if __name__ == "__main__":
     main()
