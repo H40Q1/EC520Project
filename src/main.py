dda8339f44126b6d6c7431acdcede629cc1bb70c
Haoqi Gu <55101972+H40Q1@users.noreply.github.com>
2021-04-10 15:13:06 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,24 +1,24 @@
-# EC520 Project
-# Main
-
-import sys
 import cv2
 from difference_cal import Difference
 import numpy as np
 from confusion_mat import ConfMat
 
 def main():
-    threshold = [[0.1, 0.2, 0.3, 0.4],
-                 [0.2, 0.3, 0.4, 0.5],
-                 [0.1, 0.2, 0.3, 0.4],
-                 [0.1, 0.15, 0.2, 0.3],
-                 [5, 10, 20, 40],
-                 [0.001, 0.01, 0.1, 0.2],
-                 [0.1, 0.15, 0.2, 0.3]
+    threshold = [[0.1, 0.2, 0.3, 0.4, 0.45, 0.5],
+                 [0.1, 0.2, 0.3, 0.35, 0.4, 0.45],
+                 [0.1, 0.2, 0.3, 0.35, 0.4, 0.45],
+                 [0.1, 0.15, 0.2, 0.23, 0.27, 0.3],
+                 [0.01, 0.1, 0.15, 0.2, 0.25, 0.3],
+                 [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
                  ]
 
     mat = ConfMat(threshold)
-    tpr, fpr = mat.tpr, mat.fpr
+
+    confMat, tpr, fpr = mat.resMat, mat.tpr, mat.fpr
+
+    print(confMat)
+    print(tpr)
+    print(fpr)
 
 if __name__ == "__main__":
     main()
